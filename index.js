0d89d95d40fd868d46e6fced79a6744f68555271
martinriveros <martin.riveros@hotmail.com>
2022-03-19 18:36:00 -0300

    direct link

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const fs = require('fs')
+
+let data =fs.readFileSync('./public/archivo.pdf');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.send(data))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-// setInterval(() => {
-//   fetch('')
-// }, 900000);
-
-  
